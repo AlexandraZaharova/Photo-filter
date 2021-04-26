2a34cc25ac79e3f28a09cde7620a69d9e37793b7
AlexandraZaharova <zakharova911@gmail.com>
2021-04-27 00:40:05 +0300

    feat: function handleNewFile

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -3,6 +3,7 @@ const imgContainer = document.querySelector('.img-container');
 const outputs = document.querySelectorAll('.filters output');
 let flag = 0;
 const btnNextPicture = document.querySelector('.btn-next');
+const fileInput = document.querySelector('input[type="file"]');
 
 function drawImageFromComp() {
     const canvas = document.createElement('canvas');
@@ -29,13 +30,19 @@ function handleUpdate() {
 function drawImageFirst() {
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     const img = document.querySelector('img');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
     ctx.filter = `blur(${outputs[0].value}px) 
         invert(${outputs[1].value}%)
         sepia(${outputs[2].value}%) 
         saturate(${outputs[3].value}%)
         hue-rotate(${outputs[4].value}deg)`;
     ctx.drawImage(img, 0, 0);
+    if (!canvas.hasAttribute('class')) {
+        canvas.setAttribute('class', 'first');
+    }
 }
 function drawImage(src) {
     const canvas = document.querySelector('canvas');
@@ -77,12 +84,28 @@ function getLink(flag) {
       src += `0${flag}.jpg`;
     }
     drawImage(src);
-  }
-  function handleNextPicture() {
-    flag++;
-    getLink(flag);
-  }
+}
+function handleNextPicture() {
+flag++;
+getLink(flag);
+}
+function handleNewFile(e) {
+    // let src;
+    const image = document.querySelector('img');
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+        const img = new Image();
+        img.src = reader.result;
+        image.innerHTML = "";
+        image.replaceWith(img);
+    }
+    reader.readAsDataURL(file);
+    fileInput.value = '';
+    drawImageFirst();
+}
 
 drawImageFromComp();
 inputs.forEach(input => input.addEventListener('input', handleUpdate));
 btnNextPicture.addEventListener('click', handleNextPicture);
+fileInput.addEventListener('change', handleNewFile);
