cd44ca49832a2762fd1c45eab580133d98058d26
AlexandraZaharova <zakharova911@gmail.com>
2021-04-26 22:18:02 +0300

    feat: function handleUpdate

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,5 +1,8 @@
+const inputs = document.querySelectorAll('.filters input');
+
 function drawImageFromComp() {
     const canvas = document.createElement('canvas');
+    canvas.setAttribute('class', 'first');
     const img = document.querySelector('img');
     img.after(canvas);
     const ctx = canvas.getContext('2d');
@@ -7,6 +10,32 @@ function drawImageFromComp() {
     canvas.height = img.naturalHeight;
     ctx.drawImage(img, 0, 0);
     img.setAttribute('class', 'img-none');
+    console.log(document.querySelector('canvas').getAttribute('class'));
+}
+function handleUpdate() {
+    const suffix = this.dataset.sizing;
+    this.nextElementSibling.value = this.value;
+    if (document.querySelector('canvas').getAttribute('class') == "first") {
+        drawImage(document.querySelector('img').src);
+    } 
+}
+function drawImage(src) {
+    const canvas = document.querySelector('canvas');
+    const img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    img.src = src;
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.filter = `blur(${outputs[0].value}px) 
+        invert(${outputs[1].value}%)
+        sepia(${outputs[2].value}%) 
+        saturate(${outputs[3].value}%)
+        hue-rotate(${outputs[4].value}deg)`;
+      ctx.drawImage(img, 0, 0);
+    };
 }
 
 drawImageFromComp();
+inputs.forEach(input => input.addEventListener('input', handleUpdate));
